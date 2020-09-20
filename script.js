3473e1da4ca18a8aac8e1ab09c5551560dec450c
Emirat Millenium Try <emirat.millenium@yahoo.co.id>
2020-09-21 03:17:30 +0800

    tweak scroll to top

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+//emirat punya xixixixi
+
 function scrollToTop() {
     if (document.body.scrollTop !== 0 || document.documentElement.scrollTop !== 0) {
         window.scrollBy(0, -50);
         requestAnimationFrame(scrollToTop);
     }
 }
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 500) {
+        $('.fa-arrow-up').show(300);
+    }
+    else {
+        $('.fa-arrow-up').hide(300);
+    }
+    if ($(this).scrollTop() == $(document).height() - $(window).height()) {
+        $('.fa-arrow-up').hide(300);
+    }
+});
